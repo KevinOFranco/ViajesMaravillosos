5ab25d11f87ee61a75fe79c21f284595ecd3fa70
kofranco <kofranco@bancolombia.com.co>
2023-11-16 20:33:21 -0500

    Se conecta la BD

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.uniquindio.bd2.viajesmaravillosos {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires ojdbc10;
 
     opens com.uniquindio.bd2.viajesmaravillosos to javafx.fxml;
     exports com.uniquindio.bd2.viajesmaravillosos;
