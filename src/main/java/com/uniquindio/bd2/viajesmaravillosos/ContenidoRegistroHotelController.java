09a29d832a8fe6f22c0f0b4f20fcc2caae18de42
kofranco <kofranco@bancolombia.com.co>
2023-11-16 20:13:21 -0500

    Se incluye gui registro hotel

---
+++ src/main/java/com/uniquindio/bd2/viajesmaravillosos/ContenidoRegistroHotelController.java
@@ -0,0 +1,17 @@
+package com.uniquindio.bd2.viajesmaravillosos;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ContenidoRegistroHotelController {
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    void registrar(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/com/uniquindio/bd2/viajesmaravillosos/RegistroHotelController.java
+++ src/main/java/com/uniquindio/bd2/viajesmaravillosos/RegistroHotelController.java
@@ -2,11 +2,19 @@ package com.uniquindio.bd2.viajesmaravillosos;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class RegistroHotelController {
     private Stage stage;
+
+    @FXML
+    private ScrollPane scrollPane;
 
     @FXML
     private TextField txtNombre;
@@ -14,6 +22,12 @@ public class RegistroHotelController {
     @FXML
     void registrar(ActionEvent event) {
         stage.close();
+    }
+
+    @FXML
+    void initialize() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("contenido-registro-hotel.fxml"));
+        scrollPane.setContent(fxmlLoader.load());
     }
 
     public void setStage(Stage stage) {
